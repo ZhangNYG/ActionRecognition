ea581d51ff0cd74e6b4719c98b4d46202e6bbebd
I3orn2FLY <kenessary.koishybay@nu.edu.kz>
2018-05-29 12:46:37 +0600

    ExtracData Readme

--- ExtractData/FeatExtractor.py
+++ ExtractData/FeatExtractor.py
@@ -1,31 +1,39 @@
 import cv2
 import numpy as np
 from Detection import *
-from Saver import *
+from Saver import Tracker
+import argparse
 import os
-
-
 
 if __name__ == '__main__':
 
-    directory="VideoDataset/"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_data_dir", type=str, default="VideoDataset/",
+                        help="Video Dataset folder")
+
+    args = parser.parse_args()
+    directory = args.video_data_dir
+
     with open("processed_videos.txt") as f:
         traversed = [x.strip() for x in f.readlines()]
 
+
     with open("checkpoint.txt") as f:
         video, idx = f.readline().split(" ")
+
+
     idx = int(idx)
 
-
+    dt = Detector()
     for root, dirs, files in os.walk(directory):
         for file in files:
             videopath = os.path.join(root, file)
             if file.endswith('.avi'):
 
-                if(videopath in traversed):
+                if (videopath in traversed):
                     continue
 
-                dt = Detector()
+
                 tr = Tracker()
                 vc = cv2.VideoCapture(videopath)
                 index = 0
@@ -51,18 +59,6 @@ if __name__ == '__main__':
                     file.close()
                     index += 1
 
-
                 file = open("processed_videos.txt", mode='a+')
                 file.write(videopath + "\n")
                 file.close()
-
-
-
-
-
-
-
-
-
-
-
